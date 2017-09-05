7197130243fac9367e64633b48617fea2711534a
furkan <mfg263@gmail.com>
2017-09-05 15:29:39 +0300

    filler moved into hexagonmap

--- js/hexagonmap.js
+++ js/hexagonmap.js
@@ -1,27 +1,51 @@
+import Hexagon from "./hexagon.js"
 
-var HexagonMap;
-export default HexagonMap={
-    hexagons: [],
-    index: 0,
-    rowcount: 0,
-    colcount: 0,
-    add: function (hexagon) {
-        this.hexagons[this.index++]=hexagon;
-    },
-    get: function(row, col){
-        return this.hexagons[row*this.width+col];
-    },
-    setDimensions: function(rowcount, colcount){
-        this.rowcount=rowcount; this.colcount=colcount;
-    },
-    toString: function(){
-        console.log(this.colcount);
-        var string="\n";
-        for(var i=0;i<this.rowcount;i++){
-            string+=this.hexagons.slice(i*this.colcount, (i+1)*this.colcount).join(" ");
-            string+="\n";
+export default function HexagonMap(rowc, colc){
+
+    return {
+        hexagons: [],
+        index: 0,
+        rowcount: rowc,
+        colcount: colc,
+        add: function (hexagon) {
+            this.hexagons[this.index++] = hexagon;
+        },
+        get: function (row, col) {
+            console.log(this.hexagons[row * this.width + col])
+            return this.hexagons[row * this.width + col];
+        },
+        setDimensions: function (rowcount, colcount) {
+            this.rowcount = rowcount;
+            this.colcount = colcount;
+        },
+        toString: function () {
+            console.log(this.colcount);
+            var string = "\n";
+            for (var i = 0; i < this.rowcount; i++) {
+                string += this.hexagons.slice(i * this.colcount, (i + 1) * this.colcount).join(" ");
+                string += "\n";
+            }
+            return string;
+        },
+
+        fill: function(id="container") {
+            var root=document.getElementById(id);
+            if(!root) throw new DOMException("no element with id: "+id);
+
+            for(var i = 0 ; i<this.rowcount; i++){
+                var row=document.createElement("div");
+                row.classList.add("hex-row");
+                if(i%2==1){
+                    row.classList.add("even");
+                }
+                for(var j=0; j<this.colcount; j++){
+                    var hexagon=Hexagon(true);
+                    row.appendChild(hexagon.hex);
+                    this.add(hexagon);
+                }
+                root.appendChild(row);
+            }
         }
-        return string;
     }
 
 };
--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-import fillHexagons from "./js/filler.js";
 import hexagonMap from "./js/hexagonmap.js";
 
-// module.exports = {fill: fillHexagons};
-window.fillHexagons=fillHexagons;
-window.map=hexagonMap;
+// module.exports = {???: ??????????};
+window.HexagonMap = hexagonMap;
