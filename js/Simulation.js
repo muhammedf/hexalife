7668cee618ef49458e0bbeed2522c74adae1be4c
furkan <mfg263@gmail.com>
2017-09-20 00:06:00 +0300

    simulation

---
+++ js/Simulation.js
@@ -0,0 +1,33 @@
+import {rule1} from "./Rules"
+
+export default function Simulation(hexagonmap) {
+
+    var iterating=false;
+
+    function iterate() {
+        var funcs=[];
+        for(let hex of hexagonmap.iterator()){
+            console.log("for");
+            funcs.push(rule1.apply(hex, hexagonmap.getNeighboursOf(hex).filter(n=>n.isalive).length));
+        }
+        funcs.filter(f=>f!==undefined).forEach(f=>f());
+    }
+
+    return {
+        start: function() {
+            if(!iterating){
+                iterating=true;
+                this.iterate();
+            }
+        },
+        stop: function(){
+            iterating=false;
+        },
+        iterate: function () {
+            iterate();
+            if(iterating===true){
+                setTimeout(()=>this.iterate(), 1000);
+            }
+        }
+    };
+}
